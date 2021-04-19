626701d93bbea0d2ec61421a6ac0c9546b30b4ad
= <jaskowiecpiotr@gmail.com>
2021-04-19 03:46:38 +0200

    add_sample_tests

--- project/src/test/java/pl/agh/restaurant_project/RestaurantProjectApplicationTests.java
+++ project/src/test/java/pl/agh/restaurant_project/RestaurantProjectApplicationTests.java
@@ -1,13 +1,20 @@
 package pl.agh.restaurant_project;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.agh.restaurant_project.controller.UserController;
 
 @SpringBootTest
 class RestaurantProjectApplicationTests {
 
+    @Autowired
+    private UserController controller;
+
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
     }
 
 }
