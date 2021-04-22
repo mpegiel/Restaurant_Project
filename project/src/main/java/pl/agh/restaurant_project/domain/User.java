060808a9166d39569297eb0480db6075538e01ee
dgryla <81310259+dgryla@users.noreply.github.com>
2021-04-22 19:45:25 +0200

    User - imie, nazwisko, pensja

--- project/src/main/java/pl/agh/restaurant_project/domain/User.java
+++ project/src/main/java/pl/agh/restaurant_project/domain/User.java
@@ -10,14 +10,21 @@ public class User {
     private Long personId;
     private String personLogin;
     private String personPassword;
+    private String personName;
+    private String personSurname;
+    private String personSalary;
 
     public User () {
 
     }
-    public User(Long PersonId, String PersonUsername, String PersonPassword) {
+    public User(Long PersonId, String PersonUsername, String PersonPassword, String PersonName,
+    String PersonSurname, String PersonSalary) {
         this.personId = PersonId;
         this.personLogin = PersonUsername;
         this.personPassword = PersonPassword;
+        this.personName = PersonName;
+        this.personSurname = PersonSurname;
+        this.personSalary = PersonSalary;
     }
     public Long getPersonId() {
         return personId;
@@ -25,12 +32,22 @@ public class User {
     public void setPersonId(Long id) {
         this.personId = id;
     }
-    public String getPersonLogin() {return personLogin;}
-    public void setPersonLogin(String username) {this.personLogin = username; }
+    public String getPersonLogin() {
+        return personLogin;
+    }
+    public void setPersonLogin(String username) {
+        this.personLogin = username;
+    }
     public String getPersonPassword() {
         return personPassword;
     }
     public void setPersonPassword(String password) {
         this.personPassword = password;
+    }
+    public String getPersonSalary() {
+        return personSalary;
+    }
+    public void setPersonSalary(String personSalary) {
+        this.personSalary = personSalary;
     }
 }
