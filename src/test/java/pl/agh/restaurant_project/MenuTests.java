9d8bb393e5e8d3595fb865f9821e535a4d9c5c27
mpegiel <maja@pegiel.pl>
2021-05-12 17:17:54 +0200

    [RP-152] dodanie testu menu (ilosc potraw)

---
+++ src/test/java/pl/agh/restaurant_project/MenuTests.java
@@ -0,0 +1,41 @@
+package pl.agh.restaurant_project;
+
+
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import pl.agh.restaurant_project.domain.Menu;
+import pl.agh.restaurant_project.domain.Warehouse;
+import pl.agh.restaurant_project.repository.MenuRepository;
+import pl.agh.restaurant_project.repository.WarehouseRepository;
+import pl.agh.restaurant_project.service.MenuService;
+import pl.agh.restaurant_project.service.WarehouseService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MenuTests {
+    @Autowired
+    private MenuService service;
+
+    @MockBean
+    private MenuRepository repository;
+
+    @Test
+    public void getMenuProductListSizeTests()
+    {
+        when(repository.findAll()).thenReturn(Stream.of(new Menu("Pierogi z jagodami", 15.5))
+                .collect(Collectors.toList()));
+        assertEquals(1, service.getALlMenu().size());
+    }
+
+}
